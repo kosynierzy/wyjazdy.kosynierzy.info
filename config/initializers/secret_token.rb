06d9d1aa3bcce918d4e8b124122b19aa7377ce0b
Jan Dudulski <jan@dudulski.pl>
2013-01-06 00:59:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KosynierzyInfo::Application.config.secret_token = '522b18b16d5076385dcaa3dddc4bd70c9e47a4caccfcd6ad3ec5b5ba021b9763915e644c729bae9dcce8cdd60352cb99907c50cdb6461c5bcc54d49b3b7ccc68'
