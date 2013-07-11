24b7315f9fb9482ccb1573e8f21eeb0555154c92
Jan Dudulski <jan@dudulski.pl>
2013-07-11 17:50:39 +0200

    First working acceptance test

--- spec/acceptance/tour_history.feature
+++ spec/acceptance/tour_history.feature
@@ -3,14 +3,24 @@ Feature: On Tour History
   As a user
   I want to be able to check where I have been
 
+  Background:
+    Given there are following trips:
+      | Season  | Date       | Home  | Away  | Score | Type | Number |
+      | 2000/01 | 2001-01-10 | Team1 | Team2 | 50:60 | PLK  | 15     |
+      | 2000/01 | 2001-01-15 | Team1 | Team3 | 60:50 | PLK  | 10     |
+      | 2001/02 | 2001-10-10 | Team1 | Team4 | 70:60 | PLK  | 20     |
+      | 2001/02 | 2002-02-10 | Team1 | Team5 | 50:60 | PLK  | 30     |
+
+  Scenario: Browse list
+    When I visit on tour page
+    Then I should see tour history list
+
   Scenario: Guest user lists matches
     Given I am guest user
-    And some matches has been reported
     When I visit on tour page
     Then I should not see a column about being on tour
 
   Scenario: Regular user lists matches
     Given I am signed in user
-    And some matches has been reported
     When I visit on tour page
     Then I should see a column about being on tour
---
+++ spec/steps/tour_history_steps.rb
@@ -0,0 +1,45 @@
+step "there are following trips:" do |table|
+  seasons = {}
+  matches = []
+  table.hashes.map { |h| h["Season"] }.uniq.each { |s| seasons[s] = Season.create!(name: s) }
+  table.hashes.map do |h|
+    m = Match.new do |match|
+      match.season = seasons[h["Season"]]
+      match.date = h["Date"]
+      match.home_team = h["Home"]
+      match.away_team = h["Away"]
+      match.home_score = h["Score"].match(/(\d+):/)[1]
+      match.away_score = h["Score"].match(/:(\d+)/)[1]
+      match.game_type = h["Type"]
+      match.save!
+    end
+    m.create_trip!({ official_number: h["Number"] })
+    matches << m
+  end
+
+  @matches = matches.sort_by { |m| m.date }.reverse
+end
+
+step "I am guest user" do
+end
+
+step "I visit on tour page" do
+  visit "/"
+end
+
+step "I should see tour history list" do
+  page.all(:xpath, "//table[@id='trips']//tbody//tr").each_with_index do |row, index|
+    row.find(:xpath, "td[1]").text.should eq(@matches[index].season.name)
+    row.find(:xpath, "td[3]").text.should eq(@matches[index].home_team)
+    row.find(:xpath, "td[4]").text.should eq("#{@matches[index].home_score}:#{@matches[index].away_score}")
+    row.find(:xpath, "td[5]").text.should eq(@matches[index].trip.official_number.to_s)
+  end
+end
+
+step "I should not see a column about being on tour" do
+  # todo
+end
+
+step "I should see a column about being on tour" do
+  # todo
+end
