606fc4a086ed660f74f4b1e76df39b47c6b824fa
Jan Dudulski <jan@dudulski.pl>
2013-07-19 15:59:39 +0200

    Tabularize symbols

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable,
-    :registerable,
-    :recoverable,
-    :rememberable,
-    :trackable,
-    :confirmable,
-    :lockable,
-    :validatable,
-    :timeoutable
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :confirmable,
+         :lockable,
+         :validatable,
+         :timeoutable
 
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :trips
