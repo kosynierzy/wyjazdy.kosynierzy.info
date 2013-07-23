4fad4d7ba81649d930a16cf2a04aba9fa30a8e89
Jan Dudulski <jan@dudulski.pl>
2013-07-24 01:25:42 +0200

    Fix database cleaner setup for turnip features

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -10,7 +10,11 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, :js => true) do
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each, type: :feature, javascript: true) do
     DatabaseCleaner.strategy = :truncation
   end
 
