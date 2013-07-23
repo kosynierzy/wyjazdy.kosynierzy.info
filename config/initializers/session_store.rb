f9ded63c7010976974c98404435332e3371da89f
Jan Dudulski <jan@dudulski.pl>
2013-07-24 01:23:46 +0200

    Setup test cookie domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,8 @@
 
 Kosynierzy::Application.config.session_store :cookie_store, key: '_kosynierzy.info_session', domain: {
   production: '.kosynierzy.info',
-  development: '.kosynierzy.dev'
+  development: '.kosynierzy.dev',
+  test: '.lvh.me'
 }.fetch(Rails.env.to_sym, :all)
 
 # Use the database for sessions instead of the cookie-based default,
