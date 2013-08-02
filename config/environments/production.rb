153e1e241c29d095deecf550f25b4754050f8a37
Jan Dudulski <jan@dudulski.pl>
2013-08-03 01:08:49 +0200

    Disable uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Kosynierzy::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.compress = true
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
