2f442647ac402c9c189dc1923ce0d43057599874
Jan Dudulski <jan@dudulski.pl>
2013-08-02 22:25:49 +0200

    Configure strong params for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
   end
 end
