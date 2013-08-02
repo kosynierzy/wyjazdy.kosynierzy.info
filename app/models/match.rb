71876f0db287ce0bc1f2b1f31683805bf69a109c
Jan Dudulski <jan@dudulski.pl>
2013-08-02 23:38:35 +0200

    Remove commented attr_accesible

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,8 +3,6 @@ class Match < ActiveRecord::Base
   has_one :trip, inverse_of: :match
   accepts_nested_attributes_for :trip
 
-  # attr_accessible :away_score, :away_team, :date, :game_type, :home_score, :home_team, :season_id, :won, :trip_attributes, :stats_url
-
   validates :home_team, presence: true
   validates :away_team, presence: true
   validates :date, presence: true
--- app/models/season.rb
+++ app/models/season.rb
@@ -1,7 +1,5 @@
 class Season < ActiveRecord::Base
   has_many :matches
 
-  # attr_accessible :name
-
   validates :name, presence: true, uniqueness: true
 end
