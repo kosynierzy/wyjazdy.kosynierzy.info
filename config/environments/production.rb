e48655ffc6f9ce3d72fdd976936434f4c16285fc
Jan Dudulski <jan@dudulski.pl>
2013-08-03 01:01:28 +0200

    Enable compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Kosynierzy::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
