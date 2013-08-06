450a4d1dd6f68a2d67dba646043b6442d3215889
Jan Dudulski <jan@dudulski.pl>
2013-08-06 20:18:37 +0200

    Add some specs for routing and switch hash syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,77 +1,27 @@
 Kosynierzy::Application.routes.draw do
-  constraints :subdomain => 'account' do
+  constraints subdomain: 'account' do
     devise_for :users
 
     devise_scope :user do
       authenticated :user do
-        root :to => 'devise/registrations#edit', as: :authenticated_user
+        root to: 'devise/registrations#edit', as: :authenticated_user
       end
 
-      root :to => 'devise/sessions#new', as: :guest_user
+      root to: 'devise/sessions#new', as: :guest_user
     end
   end
 
-  constraints :subdomain => 'wyjazdy' do
-    scope :module => 'on_tour' do
+  constraints subdomain: 'wyjazdy' do
+    scope module: 'on_tour' do
       resources :matches
-      resources :trips, :only => [:index] do
+      resources :trips, only: [:index], format: :json do
         post :presence
         delete :absence
       end
 
-      root :to => 'trips#index', as: :on_tour
+      root to: 'trips#index', as: :on_tour
     end
   end
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  root :to => 'on_tour/trips#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  root to: 'on_tour/trips#index'
 end
--- spec/routing/on_tour/trips_routing_spec.rb
+++ spec/routing/on_tour/trips_routing_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
-describe 'routing to on tour pages' do
+describe 'on tour trips routes' do
   it 'routes subdomain root to OnTour::Trips#index' do
-    expect({ :get => 'http://wyjazdy.kosynierzy.info/' }).to route_to(
-      :subdomain => 'wyjazdy',
-      :controller => 'on_tour/trips',
-      :action => 'index'
+    expect({ get: 'http://wyjazdy.kosynierzy.info/' }).to route_to(
+      subdomain: 'wyjazdy',
+      controller: 'on_tour/trips',
+      action: 'index'
+    )
+  end
+
+  it 'routes presence to OnTour::Trips#presence' do
+    expect({ post: 'http://wyjazdy.kosynierzy.info/trips/id/presence' }).to route_to(
+      subdomain: 'wyjazdy',
+      controller: 'on_tour/trips',
+      action: 'presence',
+      trip_id: 'id'
+    )
+  end
+
+  it 'routes absence to OnTour::Trips#absence' do
+    expect({ delete: 'http://wyjazdy.kosynierzy.info/trips/id/absence' }).to route_to(
+      subdomain: 'wyjazdy',
+      controller: 'on_tour/trips',
+      action: 'absence',
+      trip_id: 'id'
     )
   end
 end
