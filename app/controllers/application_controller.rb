2170ba96a29302da2cef5df18987d1eba494019c
Jan Dudulski <jan@dudulski.pl>
2013-08-17 03:00:31 +0200

    Better 404 handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,30 @@ class ApplicationController < ActionController::Base
     strategy DecentExposure::StrongParametersStrategy
   end
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActionView::MissingTemplate do |e|
+      logger.error(e)
+      request.format = :html
+      render_404(e)
+    end
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from ActionController::UnknownController, with: :render_404
+    rescue_from AbstractController::ActionNotFound, with: :render_404
+    rescue_from Mongoid::Errors::DocumentNotFound, with: :render_404
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
+  end
+
+  def render_404(exception)
+    @not_found_path = exception.message
+    respond_to do |format|
+      format.html { render template: 'errors/error_404.html', layout: 'layouts/application', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
   end
 end
